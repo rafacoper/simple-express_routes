05f4530bcb568ae5514d481618585ea82c85bb47
rafa-opb <rafaelbarbosa.tecnologia@outlook.com>
2023-01-27 20:48:27 -0300

    creating a token for the user

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,16 +1,21 @@
 const express = require("express")
 const mongoose = require("mongoose")
-const User = mongoose.model('User')
+const jwt = require("jsonwebtoken")
+const User = mongoose.model("User")
 
 const router = express.Router()
 
 router.post("/signup", async (req, res) => {
   const { email, password } = req.body
 
-  const user = new User({email, password}) // new instance
-  await user.save()
-
-  res.send("Made a request")
+  try {
+    const user = new User({ email, password }) // new instance
+    await user.save()
+    const token = jwt.sign({ userId: user.id }, "MY_SECRET_KEY")
+    res.send({ token })
+  } catch (err) {
+    return res.status(422).send(err.message)
+  }
 })
 
 module.exports = router
