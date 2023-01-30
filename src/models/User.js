fea662abce21bc6422621fc521c66b7f92890152
rafa-opb <rafaelbarbosa.tecnologia@outlook.com>
2023-01-30 20:25:37 -0300

    checking payload when users are logging

--- src/models/User.js
+++ src/models/User.js
@@ -33,4 +33,20 @@ userSchema.pre('save', function(next) {
   })
 })
 
+userSchema.methods.comparePassword = function(candidatePassword) {
+  const user = this
+  return new Promise((resolve, reject) => {
+  bcrypt.compare(candidatePassword, user.password, (err, isMatch) => {
+    if (err) {
+      return reject(err)
+    }
+    if(!isMatch) {
+      return reject(false)
+    }
+
+    resolve(true)
+  })
+ })
+}
+
 mongoose.model('User', userSchema)
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -11,10 +11,30 @@ router.post("/signup", async (req, res) => {
   try {
     const user = new User({ email, password }) // new instance
     await user.save()
-    const token = jwt.sign({ userId: user.id }, "MY_SECRET_KEY")
+    const token = jwt.sign({ userId: user._id }, "MY_SECRET_KEY")
     res.send({ token })
   } catch (err) {
     return res.status(422).send(err.message)
+  }
+})
+
+router.post("/signin", async (req, res) => {
+  const { email, password } = req.body
+
+  if (!email || !password) {
+    return res.status(422).send({ error: "Must provide email and password" })
+  }
+  const user = await User.findOne({ email })
+
+  if (!user) {
+    return res.status(422).send({ error: "Invalid email or password" })
+  }
+  try {
+    await user.comparePassword(password)
+    const token = jwt.sign({ userId: user._id }, "MY_SECRET_KEY")
+    res.send({ token })
+  } catch (err) {
+    return res.status(422).send({ error: "Invalid email or password" })
   }
 })
 
